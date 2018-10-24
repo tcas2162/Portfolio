3545d872c0869ca8b82ffe866b5fda82f4f97019
Tim Casey <timothy.e.casey@gmail.com>
2018-10-24 11:40:33 -0400

    updated images

--- submit.php
+++ submit.php
@@ -1,6 +1,6 @@
 <?php
-        $femail          	=       $_POST["femail"];    // MAIL	 
-        $fmessage           =       $_POST["fmessage"]; 	 //PHONE
+        $femail                 =       $_POST["femail"];    // MAIL     
+        $fmessage           =       $_POST["fmessage"];          //PHONE
 
         $message         = '<table >';
 
@@ -12,28 +12,29 @@
         $message        .= '<b>Contact us form submitted online.</b>' ;
         $message        .= '</td></tr>';
  
-		$message        .= '<tr><td>';
-        $message        .= '<b>E-MAIL ID 			:</b> '.$femail;
+                $message        .= '<tr><td>';
+        $message        .= '<b>E-MAIL ID                        :</b> '.$femail;
         $message        .= '</td></tr>';
  
         $message        .= '<tr><td>';
-        $message        .= '<b>MESSAGE 		     :</b> '.$fmessage ;
+        $message        .= '<b>MESSAGE               :</b> '.$fmessage ;
         $message        .= '</td></tr>';
 
         $message        .= '</table>';
-		
+                
 $headers = 'MIME-Version: 1.0' . "\r\n";
-//$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n"; 		// This Line is Old 
-$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";				// This Line is New 
+//$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";           // This Line is Old 
+$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";                            // This Line is New 
 // Subject
-$subject	= 'DAVID KING';
+$subject        = 'TIM CASEY';
 // To Address
-$to			= "timothy.e.casey@gmail.com";
+$to                     = "timothy.e.casey@gmail.com";
 // Additional headers
-$headers	.= 'From: '.$fname.' <'.$femail.'> ' . "\r\n";
-//$headers	.= 'Cc: <email id here>' . "\r\n";
-//$headers	.= 'Bcc: <email id here>' . "\r\n";
+$headers        .= 'From: '.$fname.' <'.$femail.'> ' . "\r\n";
+//$headers      .= 'Cc: <email id here>' . "\r\n";
+//$headers      .= 'Bcc: <email id here>' . "\r\n";
 // Mail it
 mail($to, $subject, $message, $headers);
 echo 'Thank you for your submission!';
 ?>
+
